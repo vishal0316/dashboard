7ef8255a0dd2a89dd7b3cda4c1a21036f09d99eb
vishal0316 <vr987195@gmail.com>
2024-03-13 21:08:06 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+// import { AiOutlineMenu } from "react-icons/ai";
+// import { FiShoppingCard } from "react-icons/fi";
+// import { BsChatLeft } from "react-icons/bs";
+// import { RiNotification3Line } from "react-icons/ri";
+// import { MdKeyboardArrowDown } from "react-icons/md";
+// import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+// import avatar from "../data/avatar.jpg";
+// import { Cart, Chat, Notification, UserProfile } from ".";
+import { useStateContext } from "../context/ContextProvider";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+  <TooltipComponent content={title} position="BottomCenter">
+    <button></button>
+  </TooltipComponent>
+);
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { activeMenu, setActiveMenu } = useStateContext();
+
+  return <div className="flex justify-between p-2 md:mx-6 relative "></div>;
 };
 
 export default Navbar;
